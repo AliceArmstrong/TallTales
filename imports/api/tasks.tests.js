61ac83d863be868d153b2b5a0290a64600d3972d
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-04-10 17:23:08 +0100

    Experimenting with tests

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,13 +1,51 @@
 import { Meteor } from 'meteor/meteor';
-import { assert } from 'meteor/practicalmeteor:chai'
+import { assert } from 'meteor/practicalmeteor:chai';
+
+var http = require('http');
+
+// This is just an example HTTP server. Use your own application here.
+var server = http.createServer(function(req, res) {
+	res.writeHead(200);
+	res.end('Hello, Mocha!');
+});
+
+// listen strats the server on the given port.
+exports.listen = function(port) {
+	console.log('Listening on: ' + port);
+	server.listen(port);
+};
+
+// close destroys the server.
+exports.close = function() {
+	server.close();
+};
 
 if (Meteor.isServer) {
-  describe('Tasks', () => {
-    describe('methods', () => {
+  describe('HTTP Server Test', function() {
+    before(function() {
+      server.listen(3000);
+    });
 
-      it('can delete owned task', () => {
-        assert.equal(1,1);
+    after(function() {
+      server.close();
+    });
+
+    describe('/', function() {
+      it('should be Hello Mocha!', function(done) {
+        http.get('http://127.0.0.1:3000', function(response) {
+          assert.equal(response.statusCode, 200);
+
+          var body = '';
+          response.on('data', function(d) {
+            body += d;
+          });
+
+          response.on('end', function() {
+            assert.equal(body, 'Hello, Mocha!');
+            done();
+          });
+        });
       });
     });
   });
-}
+};
